544001acdb8af679520e88516cf560c44d0aa4f9
Takahiro Noguchi <tnoguchi@umn.edu>
2026-05-11 09:11:14 -0500

    Update variables

--- source/getting-started/tutorials/autogen/simulink/setup.m
+++ source/getting-started/tutorials/autogen/simulink/setup.m
@@ -1,16 +1,16 @@
 clear; clc;
 
-fs = 10e3;      % sampling frequency (Hz)
-Ts = 1/fs;      % sampling time (sec)
-Tsim = Ts/10;   % simulation time (s) 
+samplingFrequency_Hz = 10e3;              % sampling frequency (Hz)
+samplingTime_s = 1/samplingFrequency_Hz;  % sampling time (sec)
+simulationTime_s = samplingTime_s/10;     % simulation time (s) 
 
-omega = 377.0;  % (rad/s)
-Do    = 0.8;    % (--)
+omegaElec_radps = 377.0;  % elctrical angular velocity (rad/s)
+dutyAmplitude = 0.8;      % amplitude of the duty-ratio waveform (--)
 
-%% Autogen code for the controller
-model='generateDuty';  % name of the controller to be built
+% Autogen code for the controller
+model='generate_duty';  % name of the controller to be built
 slbuild(model);      % generates the Autogen code
-oldFolder = cd('C:generateDuty_ert_rtw\');
+oldFolder = cd('C:generate_duty_ert_rtw\');
 % Copy only .c and .h files in autogen folder
 command = 'for /r %i in (*.c, *.h) do copy /y %i ..\..\..\..\firmware\project-firmware\usr\controller\autogen';
 [status, cmdout] = system(command);
