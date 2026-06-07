6cbfb1573b36021c067ac486a2705c11bc19845a
Takahiro Noguchi <tnoguchi@umn.edu>
2026-06-07 15:37:34 +0900

    Update autogen code

--- source/getting-started/tutorials/autogen/simulink/setup.m
+++ source/getting-started/tutorials/autogen/simulink/setup.m
@@ -8,11 +8,14 @@ omegaElec_radps = 377.0;  % electrical angular velocity (rad/s)
 dutyAmplitude = 0.8;      % amplitude of the duty-ratio waveform (--)
 
 % Autogen code for the controller
-model='generate_duty';  % name of the controller to be built
-slbuild(model);      % generates the Autogen code
-oldFolder = cd('C:generate_duty_ert_rtw\');
+slbuild("generate_duty");
+build_dir = "generate_duty_ert_rtw";
+dst_dir = "..\..\..\firmware\project-firmware\usr\controller\autogen";
+
+delete(fullfile(dst_dir, '*.c'));
+delete(fullfile(dst_dir, '*.h'));
+
 % Copy only .c and .h files in autogen folder
-command = 'for /r %i in (*.c, *.h) do copy /y %i ..\..\..\firmware\project-firmware\usr\controller\autogen';
-[status, cmdout] = system(command);
-cd(oldFolder);
+copyfile(fullfile(build_dir, '*.c'), dst_dir);
+copyfile(fullfile(build_dir, '*.h'), dst_dir);
 
